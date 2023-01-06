0e11d2803ebc9ceb814bcdac2d5245df93bc589b
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-01-06 09:00:40 -0300

    Cria projeto

---
+++ ByteBanckIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+var arquivo = "C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\contas.txt";
+
+var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open);
+
+var buffer = new byte[1024];
+
+var quantidadeBytesLidos = -1;
+
+while (quantidadeBytesLidos != 0)
+{
+  quantidadeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+  EscreverBuffer(buffer);
+}
+
+
+static void EscreverBuffer(byte[] buffer)
+{
+  var encoding = new UTF8Encoding();
+  var texto = encoding.GetString(buffer);
+
+  Console.Write(texto);
+
+  //foreach (var meuByte in buffer)
+  //{
+  //  Console.Write(meuByte);
+  //  Console.Write(" ");
+  //}
+}
