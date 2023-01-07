1502606519353364d7cd96b757587d48d6003db1
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-01-07 15:23:13 -0300

    Fazendo Parse e StreamWriter

---
+++ ByteBanckIO/CriandoArquivo.cs
@@ -0,0 +1,39 @@
+﻿using ByteBanckIO;
+using System.Globalization;
+using System.Text;
+
+partial class Program
+{
+  static void CriandoArquivo()
+  {
+    //CriarArquivo();
+    CriarArquivoComWriter();
+    Console.ReadLine();
+  }
+  static void CriarArquivo()
+  {
+
+    var caminhoNovoArquivo = "C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\contasExportadas.csv";
+
+    using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+    {
+      var contaComoString = "456,78945,4785.50,Gabriel Vieira";
+      var encoding = Encoding.UTF8;
+      var bytes = encoding.GetBytes(contaComoString);
+
+      fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+
+    }
+  }
+  static void CriarArquivoComWriter()
+  {
+    var caminhoNovoArquivo = "C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\contasExportadas.csv";
+
+    using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+    using (var escritor = new StreamWriter(fluxoDeArquivo))
+    {
+      escritor.Write("456,65465,456.0,Pedro Vieira");
+    }
+  }
+}
+
--- ByteBanckIO/Program.cs
+++ ByteBanckIO/Program.cs
@@ -1,32 +1,14 @@
-﻿partial class Program
+﻿using ByteBanckIO;
+using System.Globalization;
+using System.Text;
+
+partial class Program
 {
   static void Main(string[] args)
   {
     //LidandoComFileStreamDiretamente();
-
-    var enderecoDoArquivo = "C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\contas.txt";
-
-    using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-    {
-      var leitor = new StreamReader(fluxoDoArquivo);
-
-      //var linha = leitor.ReadLine();
-
-      //var texto = leitor.ReadToEnd();
-
-      //var numero = leitor.Read();
-
-      //Console.WriteLine(linha);
-      //Console.WriteLine(texto);
-      //Console.WriteLine(numero);
-
-      while (!leitor.EndOfStream)
-      {
-        var linha = leitor.ReadLine();
-        Console.WriteLine(linha);
-      }
-    }
-    Console.ReadLine();
+    //UsandoStreamReader();
+    CriandoArquivo();
   }
 }
 
