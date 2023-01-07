e0da3005d71c9c775d7fe433f9622004eb6b8124
Gabriel Vieira <gabrielvieira1@protonmail.com>
2023-01-07 16:38:55 -0300

    Streams do Console

--- ByteBanckIO/Program.cs
+++ ByteBanckIO/Program.cs
@@ -9,7 +9,33 @@ partial class Program
     //LidandoComFileStreamDiretamente();
     //UsandoStreamReader();
     //CriandoArquivo();
-    StreamBinary();
+    //StreamBinary();
+    //UsandoStreamDeEntradaDaConsole();
+
+    // Auxiliares da classe File
+
+    //Console.WriteLine("Digite seu nome:");
+    //var nome = Console.ReadLine();
+
+    var linhas = File.ReadAllLines("C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\contas.txt");
+    Console.WriteLine(linhas.Length);
+
+    /*
+    foreach (var linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+    */
+
+    var bytesArquivo = File.ReadAllBytes("C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\contas.txt");
+    Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+    File.WriteAllText("C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\escrevendoComClasseFile.txt", "Testando File.WriteAllText");
+
+    Console.WriteLine("Aplicação Finalizada ...");
+
+    Console.ReadLine();
+
   }
 }
 
---
+++ ByteBanckIO/UsandoStreamDeEntradaDaConsole.cs
@@ -0,0 +1,31 @@
+﻿using ByteBanckIO;
+using System.Globalization;
+using System.Text;
+
+partial class Program
+{
+  static void UsandoStreamDeEntradaDaConsole()
+  {
+    UsarStreamDeEntrada();
+    Console.ReadLine();
+  }
+  static void UsarStreamDeEntrada()
+  {
+    using (var fluxoDeEntrada = Console.OpenStandardInput())
+    using (var fs = new FileStream("C:\\Users\\Biel\\Documents\\workspaces\\vs\\ByteBanckIO\\entradaConsole.txt", FileMode.Create))
+    {
+      var buffer = new byte[1024]; //1kb
+
+      while (true)
+      {
+        var byteslidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+
+        fs.Write(buffer, 0, byteslidos);
+        fs.Flush();
+
+        Console.WriteLine($"Bytes lidos na console: {byteslidos}");
+      }
+    }
+  }
+}
+
